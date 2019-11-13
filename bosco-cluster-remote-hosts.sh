35c3424646c726cece129daafdcff9fa4756a5e2
Carl Edquist <edquist@cs.wisc.edu>
2019-11-13 15:43:48 -0600

    hardcoded OVERRIDE_DIR

--- bosco-cluster-remote-hosts.sh
+++ bosco-cluster-remote-hosts.sh
@@ -4,5 +4,6 @@
 [ -f /usr/share/condor-ce/condor_ce_env_bootstrap ] &&
    . /usr/share/condor-ce/condor_ce_env_bootstrap
 
+export OVERRIDE_DIR=/etc/condor-ce/bosco_override
 /usr/local/bin/bosco-cluster-remote-hosts.py
 
