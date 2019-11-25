2a103df3c99f99553d160ed1d31764c8224c381c
Brian Lin <brianhlin@gmail.com>
2019-11-25 15:26:34 -0600

    Loosen CE authZ requirements to better support k8s

--- 25-hosted-ce-setup.sh
+++ 25-hosted-ce-setup.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Condor needs to know the annoying randomly generated kubernetes pod hostname
-export GARBAGE_HOSTNAME=$(hostname -f)
-echo "SUPERUSERS = \$(SUPERUSERS), condor@daemon.htcondor.org/$GARBAGE_HOSTNAME, root@daemon.htcondor.org/$GARBAGE_HOSTNAME" \
->> /etc/condor-ce/config.d/99-hostname.conf
-echo "FRIENDLY_DAEMONS = \$(FRIENDLY_DAEMONS), condor@daemon.htcondor.org/$GARBAGE_HOSTNAME condor@child/$GARBAGE_HOSTNAME" \
->> /etc/condor-ce/config.d/99-hostname.conf 
-
 #kubernetes configmaps arent writeable
 stat /tmp/99-local.ini
 if [[ $? -eq 0 ]]; then
