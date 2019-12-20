24ee70712d526d9ced3b0996b7a63410ef26d9f9
Brian Lin <brianhlin@gmail.com>
2019-12-20 10:34:01 -0600

    Fix GridResource regex format

--- foreach_bosco_endpoint.sh
+++ foreach_bosco_endpoint.sh
@@ -4,16 +4,17 @@
 
 # for each bosco endpoint, call cmdline "$@" with batch, ruser, rhost set
 foreach_bosco_endpoint () {
-  local rx='^batch ([a-zA-Z0-9_]+) ([^ @]+)@([^ ]+)$'
+  # GridResource format: batch osguser@condor.grid.uchicago.edu condor
+  local rx='^batch ([^ @]+)@([^ ]+) ([a-zA-Z]+)$'
   local ret=0
   condor_ce_job_router_info -config |
   while read key _ val; do
     case $key in
     GridResource)
       if [[ $val =~ $rx ]]; then
-        batch=${BASH_REMATCH[1]} \
-        ruser=${BASH_REMATCH[2]} \
-        rhost=${BASH_REMATCH[3]} \
+        ruser=${BASH_REMATCH[1]} \
+        rhost=${BASH_REMATCH[2]} \
+        batch=${BASH_REMATCH[3]} \
         "$@" || ret=1
       else
         echo "'$val' was not a recognized Bosco endpoint"  # >&2
