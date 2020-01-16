1ad305cec36ce39b76ff282bb6f3b6fc01e8fb8b
Brian Lin <brianhlin@gmail.com>
2020-01-16 11:48:48 -0600

    Fix the expected dir location in the git override repo
    1. Override dir structure is expected in $RESOURCE_NAME/bosco_override
    2. Trailing slash to make sure we copy the contents of the dir

--- bosco-override-setup.sh
+++ bosco-override-setup.sh
@@ -25,6 +25,8 @@ OVERRIDE_DIR=/etc/condor-ce/bosco_override/
 
 git clone --depth=1 $GIT_ENDPOINT $REPO_DIR
 
+# Bosco override dirs are expected in the following location in the git repo:
+#   <RESOURCE NAME>/bosco_override/
 RESOURCE_DIR="$REPO_DIR/$RESOURCE_NAME/"
 [[ -d $RESOURCE_DIR ]] || errexit "Could not find $RESOURCE_NAME/ under $GIT_ENDPOINT"
-rsync -az "$REPO_DIR/$RESOURCE_NAME"  $OVERRIDE_DIR
+rsync -az "$RESOURCE_DIR/bosco_override/"  $OVERRIDE_DIR
